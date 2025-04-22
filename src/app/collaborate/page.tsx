394f770eabc3a696d54b3b067f200be2ecad7565
Rohan Sarikonda <rohan.sarikonda@gmail.com>
2025-04-22 17:36:35 -0400

    removed leftover template on collaborate page

--- src/app/collaborate/page.tsx
+++ src/app/collaborate/page.tsx
@@ -46,13 +46,6 @@ export default function Collaborate() {
                 ))}
             </ul>
             </div>
-
-
-
-        <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
-        <h1 className="text-4xl font-bold">Collaborate</h1>
-        <p className="mt-4 text-lg">We are open to collaboration.</p>
-        </div>
         </div>
     );
 }
